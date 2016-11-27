e8f9748c607db5c485b033800808c13e76b480bf
Diego <Diego@LAPTOP-MQIEK458>
2016-11-27 17:31:15 +0100

    Saludo personal

--- src/saluda/Saluda.java
+++ src/saluda/Saluda.java
@@ -1,11 +1,17 @@
 package saluda;
 
+import java.util.Scanner;
+
 public class Saluda {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hola");
-
+		Scanner teclado = new Scanner(System.in);		
+		System.out.println("¿Cómo te llamas?");		
+		String nombre = teclado.nextLine();		
+		System.out.println("Hola " + nombre);
+		teclado.close();
+		
 	}
 
 }
